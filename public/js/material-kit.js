31974dc1c304a92d2d4722201543f0f0b1a59e01
unknown <belalsukari@gmail.com>
2020-12-27 02:02:04 +0100

    nav.

--- public/js/material-kit.js
+++ public/js/material-kit.js
@@ -57,14 +57,14 @@ $(document).on('click', '.navbar-toggler', function() {
   $toggle = $(this);
 
   if (materialKit.misc.navbar_menu_visible == 1) {
-    $('html').removeClass('nav-open');
+    $('body').removeClass('nav-open');
     materialKit.misc.navbar_menu_visible = 0;
     $('#bodyClick').remove();
     setTimeout(function() {
       $toggle.removeClass('toggled');
     }, 550);
 
-    $('html').removeClass('nav-open-absolute');
+    $('body').removeClass('nav-open-absolute');
   } else {
     setTimeout(function() {
       $toggle.addClass('toggled');
@@ -73,10 +73,10 @@ $(document).on('click', '.navbar-toggler', function() {
 
     div = '<div id="bodyClick"></div>';
     $(div).appendTo("body").click(function() {
-      $('html').removeClass('nav-open');
+      $('body').removeClass('nav-open');
 
       if ($('nav').hasClass('navbar-absolute')) {
-        $('html').removeClass('nav-open-absolute');
+        $('body').removeClass('nav-open-absolute');
       }
       materialKit.misc.navbar_menu_visible = 0;
       $('#bodyClick').remove();
